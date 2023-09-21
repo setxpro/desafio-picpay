d229ff0f773b3801c94a33adf99c58ecf80d8d83
Patrick Anjos <patrickpqdt87289@gmail.com>
2023-09-21 20:19:31 -0300

    feat: interface transaction repository

--- src/main/java/com/setxpro/picpay/repositories/TransactionRepository.java
+++ src/main/java/com/setxpro/picpay/repositories/TransactionRepository.java
@@ -1,6 +1,6 @@
 package com.setxpro.picpay.repositories;
 
-import com.setxpro.picpay.transaction.Transaction;
+import com.setxpro.picpay.domain.transaction.Transaction;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface TransactionRepository extends JpaRepository<Transaction, Long> {
