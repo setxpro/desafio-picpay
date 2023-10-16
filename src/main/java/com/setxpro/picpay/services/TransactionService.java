aeba7d7785fc363a18ef6ef5ac1842dec8ef2968
Patrick Anjos <patrickpqdt87289@gmail.com>
2023-10-16 11:59:23 -0300

    remove: authorization Transaction

--- src/main/java/com/setxpro/picpay/services/TransactionService.java
+++ src/main/java/com/setxpro/picpay/services/TransactionService.java
@@ -1,21 +1,19 @@
 package com.setxpro.picpay.services;
 
+import com.setxpro.picpay.domain.EmailSendUseCase;
 import com.setxpro.picpay.domain.user.User;
 import com.setxpro.picpay.dtos.TransactionDTO;
 import com.setxpro.picpay.repositories.TransactionRepository;
 import com.setxpro.picpay.domain.transaction.Transaction;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
 
-import java.math.BigDecimal;
 import java.time.LocalDateTime;
-import java.util.Map;
 
 @Service
 public class TransactionService {
+
+  //  private EmailSendUseCase emailSendUseCase;
 
     @Autowired
     private UserService userService;
@@ -25,10 +23,11 @@ public class TransactionService {
     // Requisição
 
     @Autowired
-    private RestTemplate restTemplate;
+    private AuthorizationService authorizationService;
 
     @Autowired
     private NotificationService notificationService;
+
 
     public Transaction createTransaction(TransactionDTO transaction) throws Exception {
         User sender = this.userService.findUserById(transaction.senderId());
@@ -42,10 +41,10 @@ public class TransactionService {
 
         // Se não está autorizado.
 
-//        boolean isAuthorized = this.authorizationTransaction(sender, transaction.value());
-//        if(!isAuthorized) {
-//            throw new Exception("Transação não autorizada.");
-//        }
+        boolean isAuthorized = this.authorizationService.authorizationTransaction(sender, transaction.value());
+        if(!isAuthorized) {
+            throw new Exception("Transação não autorizada.");
+       }
 
         // Se Autorizado.
 
@@ -56,12 +55,10 @@ public class TransactionService {
         newTransaction.setTimestamp(LocalDateTime.now());
 
         // Save database
-
         sender.setBalance(sender.getBalance().subtract(transaction.value()));
         receiver.setBalance(receiver.getBalance().add(transaction.value()));
 
         // Persisted data on database
-
         repository.save(newTransaction);
         userService.saveUser(sender);
         userService.saveUser(receiver);
@@ -70,21 +67,16 @@ public class TransactionService {
         this.notificationService.sendNotification(sender, "Transação realizada com sucesso!");
         this.notificationService.sendNotification(receiver, "Transação recebida com sucesso!");
 
+        // Interface
+     //   emailSendUseCase.sendEmail("Transação realizada "+ "no valor de R$"+ transaction.value() + "para " + receiver.getFirstName() +" "+ receiver.getLastName() + " com sucesso!", sender.getEmail(), "Transação.", transaction.value(), sender.getFirstName());
+     //   emailSendUseCase.sendEmail("Transação recebida com sucesso!", receiver.getEmail(), "Transação.", transaction.value(), receiver.getFirstName());
+
+        // Sem interface
         ServiceEmail.Sender("Transação realizada "+ "no valor de R$"+ transaction.value() + "para " + receiver.getFirstName() +" "+ receiver.getLastName() + " com sucesso!", sender.getEmail(), "Transação.", transaction.value(), sender.getFirstName());
         ServiceEmail.Sender("Transação recebida com sucesso!", receiver.getEmail(), "Transação.", transaction.value(), receiver.getFirstName());
         return newTransaction;
 
     }
 
-    public boolean authorizationTransaction(User sender, BigDecimal value) {
-       ResponseEntity<Map> authorizationResponse = restTemplate.getForEntity("https://run.mocky.io/v3/8fafdd68-a090-496f-8c9a-3442cf30dae6", Map.class);
-
-       if (authorizationResponse.getStatusCode() == HttpStatus.OK) {
-           String message = (String) authorizationResponse.getBody().get("message");
-            return "Autorizado.".equalsIgnoreCase(message);
-       }
-
-       return false;
-    }
 
 }
