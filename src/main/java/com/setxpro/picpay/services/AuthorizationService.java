d861a92fecf9199ed6399bf4077aa43c29633ce2
Patrick Anjos <patrickpqdt87289@gmail.com>
2023-10-16 11:57:28 -0300

    refactor: create authorization service outside the transaction class

---
+++ src/main/java/com/setxpro/picpay/services/AuthorizationService.java
@@ -0,0 +1,29 @@
+package com.setxpro.picpay.services;
+
+import com.setxpro.picpay.domain.user.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.math.BigDecimal;
+import java.util.Map;
+
+@Service
+public class AuthorizationService {
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    public boolean authorizationTransaction(User sender, BigDecimal value) {
+        ResponseEntity<Map> authorizationResponse = restTemplate.getForEntity("https://run.mocky.io/v3/8fafdd68-a090-496f-8c9a-3442cf30dae6", Map.class);
+
+        if (authorizationResponse.getStatusCode() == HttpStatus.OK) {
+            String message = (String) authorizationResponse.getBody().get("message");
+            return "Autorizado.".equalsIgnoreCase(message);
+        }
+
+        return false;
+    }
+}
